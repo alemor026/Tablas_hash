b7adcaccb520b14f31043b6bd8af8f3d248ce472
alemor026 <78941341+alemor026@users.noreply.github.com>
2022-05-01 19:12:28 -0600

    Tablas Hash

---
+++ tablas_hash/src/tablas_hash/Analisis.java
@@ -0,0 +1,28 @@
+package tablas_hash;
+
+/**
+ *
+ * @author AleMor
+ */
+public class Analisis {
+    
+    private Principal tipo;
+    private String valor;
+
+    public Principal getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Principal tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+}
---
+++ tablas_hash/src/tablas_hash/analizador.java
@@ -0,0 +1,18 @@
+package tablas_hash;
+
+/**
+ *
+ * @author AleMor
+ 
+ */
+public class analizador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Inicio().setVisible(true);
+    }
+    
+}
